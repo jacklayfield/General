492a56b464254b9979c0905c9b17fffa3a198f59
jacklayfield <layf100@gmail.com>
2023-09-25 23:03:42 -0400

    Added store and Tailwind

--- Vue/vite-project/src/App.vue
+++ Vue/vite-project/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <router-view />
+  <h1 className="text-3xl font-bold underline">Hello world!</h1>
 </template>
 
 <script setup></script>
--- Vue/vite-project/src/views/Home.vue
+++ Vue/vite-project/src/views/Home.vue
@@ -1,3 +1,12 @@
-<template>Home12131414</template>
+<template>
+  <div class="p-8 pb-0 text-orange-500">
+    <h1 class="text-4xl font-bold mb-4">Example</h1>
+  </div>
+  <Meals :meals="meals" />
+</template>
 
-<script setup></script>
+<script setup>
+import { computed } from "vue";
+import store from "../store";
+const meals = computed(() => store.state.meals);
+</script>
