165940964dc9ce4fde81bcf018d41f9a3f2c9d3f
jacklayfield <layf100@gmail.com>
2023-10-01 20:50:08 -0400

    Search by name completed

---
+++ Vue/vite-project/src/components/MealItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="bg-white shadow rounded-xl hover:scale-105 transition-all">
+    <router-link :to="{ name: 'home' }">
+      <img
+        :src="meal.strMealThumb"
+        :alt="meal.strMeal"
+        class="rounded-t-xl w-full h-48 object-cover"
+      />
+    </router-link>
+    <div class="p-3">
+      <h3 class="font-bold">{{ meal.strMeal }}</h3>
+      <p class="mb-4">
+        {{ $filters.truncateWords(meal.strInstructions, 20) }}
+      </p>
+      <div class="flex items-center justify-between"></div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const { meal } = defineProps({
+  meal: {
+    required: true,
+    type: Object,
+  },
+});
+</script>
--- Vue/vite-project/src/views/MealsByName.vue
+++ Vue/vite-project/src/views/MealsByName.vue
@@ -1,23 +1,30 @@
 <template>
-  <div class="p-6">
+  <div class="p-8 pb-0">
+    <h1 class="text-4xl font-bold mb-4 text-orange-500">
+      Search Meals by Name
+    </h1>
+  </div>
+  <div class="px-8 pb-3">
     <input
       type="text"
       v-model="keyword"
-      className="rounded border-2 bg-white border-gray-200 w-full h-12 pl-2"
+      class="rounded border-2 bg-white border-gray-200 focus:ring-orange-500 focus:border-orange-500 w-full"
       placeholder="Search for Meals"
       @change="searchMeals"
     />
   </div>
-  <div>
-    <pre>{{ meals }}</pre>
-  </div>
+
+  <Meals :meals="meals" />
 </template>
 
 <script setup>
 import { computed } from "@vue/reactivity";
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
 import store from "../store";
+import Meals from "../components/Meals.vue";
 
+const route = useRoute();
 const keyword = ref("");
 const meals = computed(() => store.state.searchedMeals);
 
@@ -28,4 +35,11 @@ function searchMeals() {
     store.commit("setSearchedMeals", []);
   }
 }
+
+onMounted(() => {
+  keyword.value = route.params.name;
+  if (keyword.value) {
+    searchMeals();
+  }
+});
 </script>
